768313ebb1672f6ddad80c14dbe228ad03d1233f
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-10-19 00:21:08 +0900

    chap6

--- app/src/main/java/jp/techacademy/yusuke/iwaki/ta_lesson6/MainActivity.java
+++ app/src/main/java/jp/techacademy/yusuke/iwaki/ta_lesson6/MainActivity.java
@@ -1,8 +1,10 @@
 package jp.techacademy.yusuke.iwaki.ta_lesson6;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +13,6 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
@@ -19,6 +20,8 @@ import io.realm.RealmResults;
 import io.realm.Sort;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.taro.kirameki.taskapp.TASK";
+
     private Realm mRealm;
     private RealmResults<Task> mTaskRealmResults;
     private RealmChangeListener mRealmListener = new RealmChangeListener() {
@@ -39,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -59,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // 入力・編集する画面に遷移させる
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task);
+
+                startActivity(intent);
             }
         });
 
@@ -68,15 +77,34 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
                 // タスクを削除する
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.clear();
+                        mRealm.commitTransaction();
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
                 return true;
             }
         });
-
-        if (mTaskRealmResults.size() == 0) {
-            // アプリ起動時にタスクの数が0であった場合は表示テスト用のタスクを作成する
-            addTaskForTest();
-        }
 
         reloadListView();
     }
@@ -106,17 +134,6 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         mRealm.close();
-    }
-
-    private void addTaskForTest() {
-        Task task = new Task();
-        task.setTitle("作業");
-        task.setContents("プログラムを書いてPUSHする");
-        task.setDate(new Date());
-        task.setId(0);
-        mRealm.beginTransaction();
-        mRealm.copyToRealmOrUpdate(task);
-        mRealm.commitTransaction();
     }
 
     @Override
